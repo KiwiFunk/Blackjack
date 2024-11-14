494a1b242c99e7dd96862ab06915526ddda3635e
KiwiFunk <alex_p97@live.com>
2024-11-14 23:46:03 +0000

    Player Init Created
    Created the basic player array initialization to take input names and pass them into a player array, then give the option to start the game. Needs to be restructured into a method.

--- Program.cs
+++ Program.cs
@@ -3,24 +3,131 @@ int[,] cardDeck = new int[,]
     { { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 },
     { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 } };
 
-int totalCardsRemaining = 52;
+//int totalCardsRemaining = 52;
 
 Random cardSelect = new Random();
 
-int currentTurn = 0;
+//int currentTurn = 0;
 string? returnResult;
 
-int[] playerHand;
-int[] dealerHand;
-double bank = 00.00;
 
-Console.WriteLine(drawCard(cardDeck));
+string[] players = {};                                //Array to store players
+//int[,] playersHand;                                 //2D array to store current players hands. row= player, column = their cards init using players[].Length to decide x [x,]
+//int[] dealerHand;
+//double bank = 00.00;
+
+bool gameStart = false;
+string playerNames = "";
+
+Console.WriteLine("Weclome to BlackJack!");
+Console.WriteLine("Please enter your Player Name(s) then hit return. Type Start to begin");
+do
+{
+    
+    returnResult = Console.ReadLine();
+    if (returnResult != null)
+    {
+        if (returnResult.ToLower().Trim() == "start")
+        {
+            if (playerNames == "")
+            {
+                 Console.WriteLine("You need to enter at least one name before starting!");
+                 continue;
+            }
+            else 
+            {
+                //Create an array out of the playerNames list
+                players = playerNames.Trim().ToUpper().Split(" ");     //Trim whitespace from start and end, create an array from player names then set
+                gameStart = true;
+                // break;
+            }
+        }
+        else
+        {
+            playerNames += returnResult.Trim() + " ";
+            Console.WriteLine($"{returnResult} Registered. Enter another name, or type start to begin!");
+        }
+
+    }
+
+}while (gameStart == false);
+
+foreach (string player in players)
+{
+    Console.WriteLine($"{player}");
+}
+
+
+
+//init total players - How can we do this?
+
+/*
+Prompt user to enter player names, type start to begin.
+For each string entered that isnt null, add to an array of players
+We can then iterate through the player array in the main() loop
+
+best to use a foreach as we dont know how many players will be playing
+
+foreach (player in players)
+{
+    Game logic here
+}
+
+Basic blackjack rules
+
+everyone gets two cards, the dealer shows one card, the other is face down
+reveal the face down card once the player has folded
+
+
+*/
+
+
+//Promt enter player names
+
+
+
+
+//Main - Player first time?
+
+//if yes
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine(drawCard(cardDeck));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
     Each player gets 2 cards. the dealer gets two cards (one remains face down). Player can chsoe to hit or stand. The dealer keeps drawing untill they 
     reach 17 or more. Or in this case, if the player stands, they will stop if their total is higher.
 */
+
+/*
 
 int drawCard(int[,] cardDeck)
 {
@@ -34,7 +141,7 @@ int drawCard(int[,] cardDeck)
         if (draw != 0)
         {
             cardDeck[suit, card] = 0;
-            totalCardsRemaining --;
+            totalCardsRemaining--;
             validCard = true;
         }
         else
@@ -48,6 +155,6 @@ int drawCard(int[,] cardDeck)
     return draw;
 }
 
-
+*/
 
 
