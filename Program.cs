fc2540232764cf3e79e1bf84c2616c1b2d2f69df
Alex <alex_p97@live.com>
2024-11-17 00:04:48 +0000

    Started on Card Dealing method
    Finish up later

--- Program.cs
+++ Program.cs
@@ -8,26 +8,23 @@ int[,] cardDeck = new int[,]
     { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 } };
 
 int totalCardsRemaining = 52;
-
 Random cardSelect = new Random();
-
-//int currentTurn = 0;
 string? returnResult;
 
-
-string[] players = { };                                //Array to store players
-//int[,] playersHand;                                 //2D array to store current players hands. row= player, column = their cards init using players[].Length to decide x [x,]
-//int[] dealerHand;
-double[,] bank = new double[0,0];
+string[] players = { };                             //Array to store players
+int[,] playersHand;                               //2D array to store current players hands. row= player, column = their cards init using players[].Length to decide x [x,]
+int[] dealerHand;
+double[,] bank = new double[0,0];                   //Using a 2D array to store money. Each row(player) has 2 columns, Index 0 holds bank balance, 1 Holds the current bet amount.
 int round = 0;
 
 bool gameStart = false;
 string playerNames = "";
 
 
-//Potentially implement an option for player limit, with 5 or 7 being the max. 4 may be better for the sake of a conputer game.
+//Potentially implement an option for player limit, with 5 or 7 being the max. 4 may be better for the sake of a computer game.
 Console.WriteLine("Weclome to BlackJack!");
 InitializePlayers();
+Betting();
 
 /*
 foreach (string player in players)
@@ -37,28 +34,41 @@ foreach (string player in players)
 }
 */
 
-Betting();
 
-/*
-foreach (string player in players)
+void AssignCards(string switchCase)
 {
-    Console.WriteLine($"{player} drew a 4");
-    //call the dealcards option for each player and populate the playersHand[,] array.
+    switch(switchCase)
+    {
+        case "FirstRound":
+        for (int i = 0; i < players.Length; i++)
+        {
+            //create new 
+
+        }
+        
+        
+        //If the first round is called, we need to give everyone TWO cards, and then make sure that one of the dealers is hidden (The dealer only reveals the hidden card after the players all fold.)
+        break;
+
+        case "player":
+        //Same as the dealer logic, only with an additional check to make sure that the correct player hand is updated
+        break;
+
+        case "dealer":
+        //Most basic Logic. get a card, add to hand, run the evaluateWinConditions() method.
+        break;
+    }
 }
-*/
-/*
-if (round = 0)
-{
-    AssignCards("FirstRound");
-}
-*/
+
+
+
+
 
 void Betting()
 {
     bool validEnty = false;
     double currentWager = 0.0;
-    //if bank 2D array is currently not instanced.
-    if (bank.Length == 0)
+    if (bank.Length == 0)            //Check to see if game is a new game, or continuation
     {
         double[,] bankInit = new double[players.Length, 2];
         bank = bankInit;
@@ -66,11 +76,9 @@ void Betting()
         {
             bank[i, 0] = 10.00;      //Starting Bank balance   
             bank[i, 1] = 0.00;       //Current Bet Amount
-
         }
         if (players.Length <= 1) Console.WriteLine("You have been given $10.00 as a new player bonus!");
         else Console.WriteLine("You have each been given a new player bonus of $10.00!");
-        
     }
     for (int i = 0; i < players.Length; i++)
     {
@@ -100,37 +108,6 @@ void Betting()
 }
 
 
-
-/*
-void AssignCards(string switchCase)
-{
-    switch(switchCase)
-    {
-        case "FirstRound":
-        
-        
-        //If the first round is called, we need to give everyone TWO cards, and then make sure that one of the dealers is hidden (The dealer only reveals the hidden card after the players all fold.)
-        break;
-
-        case "player":
-        //Same as the dealer logic, only with an additional check to make sure that the correct player hand is updated
-        break;
-
-        case "dealer":
-        //Most basic Logic. get a card, add to hand, run the evaluateWinConditions() method.
-        break;
-    }
-}
-
-*/
-
-
-
-
-
-
-
-
 int DrawCard(int[,] cardDeck)
 {
     int suit = cardSelect.Next(1, 4);
@@ -155,6 +132,7 @@ int DrawCard(int[,] cardDeck)
     } while (validCard == false);
     return draw;
 }
+
 
 void InitializePlayers()
 {
@@ -187,57 +165,3 @@ void InitializePlayers()
 
     } while (gameStart == false);
 }
-
-
-
-
-
-
-
-
-
-
-
-//init total players - How can we do this?
-
-/*
-Prompt user to enter player names, type start to begin.
-For each string entered that isnt null, add to an array of players
-We can then iterate through the player array in the main() loop
-
-best to use a foreach as we dont know how many players will be playing
-
-foreach (player in players)
-{
-    Game logic here
-}
-
-Basic blackjack rules
-
-everyone gets two cards, the dealer shows one card, the other is face down
-reveal the face down card once the player has folded
-
-
-*/
-
-
-//Promt enter player names
-
-
-
-
-//Main - Player first time?
-
-//if yes
-
-
-// Console.WriteLine(drawCard(cardDeck));
-
-
-/*
-    Each player gets 2 cards. the dealer gets two cards (one remains face down). Player can chsoe to hit or stand. The dealer keeps drawing untill they 
-    reach 17 or more. Or in this case, if the player stands, they will stop if their total is higher.
-*/
-
-
-
